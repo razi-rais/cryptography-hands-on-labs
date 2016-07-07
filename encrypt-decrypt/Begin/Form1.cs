b3e550914c336983de4b9965891afc17de2d253a
Razi Rais <razibinrais@outlook.com>
2016-07-07 06:51:28 +0000

    Lab - Encrypt & Decrypt Content
    In this lab you will learn how to encrypt and decrypt content using .NET class libraries.  
    
    This walkthrough uses the following guidelines for encryption:
    
    •	Use the RijndaelManaged class, a symmetric algorithm, to encrypt and decrypt data by using its automatically generated Key and IV.
    •	Use the RSACryptoServiceProvider, an asymmetric algorithm, to encrypt and decrypt the key to the data encrypted by RijndaelManaged. Asymmetric algorithms are best used for smaller amounts of data, such as a key.
    
    NOTE: This code is based on MSDN article: https://msdn.microsoft.com/en-us/library/bb397867(v=vs.110).aspx .I have to fix few bugs to get everything to work but teste

---
+++ encrypt-decrypt/Begin/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cryptography
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEncryptFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDecryptFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCreateAsmKeys_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonImportPublicKey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExportPublicKey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGetPrivateKey_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ encrypt-decrypt/End/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Cryptography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.buttonImportPublicKey = new System.Windows.Forms.Button();
+            this.buttonGetPrivateKey = new System.Windows.Forms.Button();
+            this.buttonDecryptFile = new System.Windows.Forms.Button();
+            this.buttonCreateAsmKeys = new System.Windows.Forms.Button();
+            this.buttonExportPublicKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.Location = new System.Drawing.Point(12, 66);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncryptFile.TabIndex = 0;
+            this.buttonEncryptFile.Text = "Encrypt File";
+            this.buttonEncryptFile.UseVisualStyleBackColor = true;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // buttonImportPublicKey
+            // 
+            this.buttonImportPublicKey.Location = new System.Drawing.Point(251, 66);
+            this.buttonImportPublicKey.Name = "buttonImportPublicKey";
+            this.buttonImportPublicKey.Size = new System.Drawing.Size(106, 23);
+            this.buttonImportPublicKey.TabIndex = 1;
+            this.buttonImportPublicKey.Text = "Import Public Key";
+            this.buttonImportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonImportPublicKey.Click += new System.EventHandler(this.buttonImportPublicKey_Click);
+            // 
+            // buttonGetPrivateKey
+            // 
+            this.buttonGetPrivateKey.Location = new System.Drawing.Point(473, 66);
+            this.buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            this.buttonGetPrivateKey.Size = new System.Drawing.Size(108, 23);
+            this.buttonGetPrivateKey.TabIndex = 2;
+            this.buttonGetPrivateKey.Text = "Get Private Key";
+            this.buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            this.buttonGetPrivateKey.Click += new System.EventHandler(this.buttonGetPrivateKey_Click);
+            // 
+            // buttonDecryptFile
+            // 
+            this.buttonDecryptFile.Location = new System.Drawing.Point(93, 66);
+            this.buttonDecryptFile.Name = "buttonDecryptFile";
+            this.buttonDecryptFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecryptFile.TabIndex = 3;
+            this.buttonDecryptFile.Text = "Decrypt File";
+            this.buttonDecryptFile.UseVisualStyleBackColor = true;
+            this.buttonDecryptFile.Click += new System.EventHandler(this.buttonDecryptFile_Click);
+            // 
+            // buttonCreateAsmKeys
+            // 
+            this.buttonCreateAsmKeys.Location = new System.Drawing.Point(171, 66);
+            this.buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            this.buttonCreateAsmKeys.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreateAsmKeys.TabIndex = 4;
+            this.buttonCreateAsmKeys.Text = "Create Keys";
+            this.buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            this.buttonCreateAsmKeys.Click += new System.EventHandler(this.buttonCreateAsmKeys_Click);
+            // 
+            // buttonExportPublicKey
+            // 
+            this.buttonExportPublicKey.Location = new System.Drawing.Point(363, 66);
+            this.buttonExportPublicKey.Name = "buttonExportPublicKey";
+            this.buttonExportPublicKey.Size = new System.Drawing.Size(104, 23);
+            this.buttonExportPublicKey.TabIndex = 5;
+            this.buttonExportPublicKey.Text = "Export Public Key";
+            this.buttonExportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonExportPublicKey.Click += new System.EventHandler(this.buttonExportPublicKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 140);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExportPublicKey);
+            this.Controls.Add(this.buttonCreateAsmKeys);
+            this.Controls.Add(this.buttonDecryptFile);
+            this.Controls.Add(this.buttonGetPrivateKey);
+            this.Controls.Add(this.buttonImportPublicKey);
+            this.Controls.Add(this.buttonEncryptFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.Button buttonImportPublicKey;
+        private System.Windows.Forms.Button buttonGetPrivateKey;
+        private System.Windows.Forms.Button buttonDecryptFile;
+        private System.Windows.Forms.Button buttonCreateAsmKeys;
+        private System.Windows.Forms.Button buttonExportPublicKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
